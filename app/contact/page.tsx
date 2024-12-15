8fd547882e1cd05af4b2f4dc6fa4c39e5ae2f623
Pablojohn Martinez <pj@Pablojohns-MacBook-Pro.local>
2024-12-15 10:31:42 -0600

    wip: initial copy of the forked chronark

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -7,21 +7,21 @@ import { Card } from "../components/card";
 const socials = [
 	{
 		icon: <Twitter size={20} />,
-		href: "https://twitter.com/chronark_",
+		href: "https://twitter.com/pablojohn_",
 		label: "Twitter",
-		handle: "@chronark_",
+		handle: "@pablojohn_",
 	},
 	{
 		icon: <Mail size={20} />,
-		href: "mailto:dev@chronark.com",
+		href: "mailto:dev@pablojohn.dev",
 		label: "Email",
-		handle: "dev@chronark.com",
+		handle: "dev@pablojohn.dev",
 	},
 	{
 		icon: <Github size={20} />,
-		href: "https://github.com/chronark",
+		href: "https://github.com/pablojohn",
 		label: "Github",
-		handle: "chronark",
+		handle: "pablojohn",
 	},
 ];
 
--- app/layout.tsx
+++ app/layout.tsx
@@ -6,19 +6,19 @@ import { Analytics } from "./components/analytics";
 
 export const metadata: Metadata = {
   title: {
-    default: "chronark.com",
-    template: "%s | chronark.com",
+    default: "pablojohn.dev",
+    template: "%s | pablojohn.dev",
   },
   description: "Co-founder of unkey.dev and founder of planetfall.io",
   openGraph: {
-    title: "chronark.com",
+    title: "pablojohn.dev",
     description:
       "Co-founder of unkey.dev and founder of planetfall.io",
-    url: "https://chronark.com",
-    siteName: "chronark.com",
+    url: "https://pablojohn.dev",
+    siteName: "pablojohn.dev",
     images: [
       {
-        url: "https://chronark.com/og.png",
+        url: "https://pablojohn.dev/og.png",
         width: 1920,
         height: 1080,
       },
@@ -38,7 +38,7 @@ export const metadata: Metadata = {
     },
   },
   twitter: {
-    title: "Chronark",
+    title: "pablojohn",
     card: "summary_large_image",
   },
   icons: {
