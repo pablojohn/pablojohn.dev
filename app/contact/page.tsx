680a3fe8498965f2c67acda846e82e00f3875881
Pablojohn Martinez <pj@Pablojohns-MacBook-Pro.local>
2024-12-15 14:05:48 -0600

    keeping particles on the contact page - adjust nav to not link to other pages

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,8 +1,9 @@
 "use client";
-import { Github, Mail, Twitter, Linkedin } from "lucide-react";
+import { Github, Twitter, Linkedin } from "lucide-react";
 import Link from "next/link";
 import { Navigation } from "../components/nav";
 import { Card } from "../components/card";
+import Particles from "../components/particles";
 
 const socials = [
 	{
@@ -27,7 +28,7 @@ const socials = [
 
 export default function Example() {
 	return (
-		<div className=" bg-gradient-to-tl from-zinc-900/0 via-zinc-900 to-zinc-900/0">
+		<div className="bg-gradient-to-tl from-zinc-900/0 via-zinc-900 to-zinc-900/0">
 			<Navigation />
 			<div className="container flex items-center justify-center min-h-screen px-4 mx-auto">
 				<div className="grid w-full grid-cols-1 gap-8 mx-auto mt-32 sm:mt-0 sm:grid-cols-3 lg:gap-16">
@@ -58,6 +59,10 @@ export default function Example() {
 					))}
 				</div>
 			</div>
+			<Particles
+				className="absolute inset-0 -z-10"
+				quantity={100}
+			/>
 		</div>
 	);
 }
